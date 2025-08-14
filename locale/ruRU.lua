560367c6b03c03d7ee2ebf2ee4b6a39353b6c055
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-08-15 03:02:45 +0500

    Update ruRU.lua

--- locale/ruRU.lua
+++ locale/ruRU.lua
@@ -3,6 +3,6 @@ local _, ExpansionUtils = ...
 ExpansionUtils:AddTrans("ruRU", "LID_GREATVAULT", "Великое Хранилище")
 ExpansionUtils:AddTrans("ruRU", "LID_RESHIIWRAP", "Повязки реши")
 ExpansionUtils:AddTrans("ruRU", "LID_TOGGLEGREATVAULT", "Вкл./выкл. Великое Хранилище")
-ExpansionUtils:AddTrans("ruRU", "LID_TOGGLERESHIIWRAP", "Вкл./выкл. Мастер повязок реши")
+ExpansionUtils:AddTrans("ruRU", "LID_TOGGLERESHIIWRAP", "Вкл./выкл. Улучшение Повязки реши")
 ExpansionUtils:AddTrans("ruRU", "LID_SHOWVAULTMMBTN", "Показывать иконку Великого Хранилища на миникарте")
 ExpansionUtils:AddTrans("ruRU", "LID_MMBTNRESHIIWRAP", "Показывать иконку Повязки реши на миникарте")
