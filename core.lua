895dbe10a96d5cd09bf2aff095056cce64e80a72
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-19 18:42:19 +0200

    v1.2.42

--- core.lua
+++ core.lua
@@ -100,7 +100,7 @@ ExpansionUtils:RegisterEvent(fEV, "PLAYER_LOGIN")
 ExpansionUtils:OnEvent(fEV, function()
 	ExpansionUtils:UnregisterEvent(fEV, "PLAYER_LOGIN")
 	ExpansionUtils:SetAddonOutput("ExpansionUtils", 133740)
-	ExpansionUtils:SetVersion(133740, "1.2.41")
+	ExpansionUtils:SetVersion(133740, "1.2.42")
 	EVTAB = EVTAB or {}
 	if EVTAB["MMBtnReshiWrap"] == nil then
 		EVTAB["MMBtnReshiWrap"] = EVTAB["MMBtnReshiWrap"] or {}
